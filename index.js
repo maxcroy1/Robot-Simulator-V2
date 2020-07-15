1bb612807dc83fdbfe22ea7058dd9df73b73cdd0
maxcroy1 <max.croy1@gmail.com>
2020-07-15 10:51:16 -0400

    Added event listener allowing us to track user input, and set up functionality for move button

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
-  createGrid()
-  renderBot(currentPosition)
+  createGrid();
+  renderBot(currentPosition);
 
   // ADD CODE HERE!
-})
+  const moveList = document.getElementById('moves-container');
+  const moveButton = document.getElementById('move-button');
+  let userMoves = new Array;
+
+  function executeMove() {
+    let direction = userMoves.shift();
+    move(direction);
+    moveList.firstChild.remove();
+  };
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight") {
+      userMoves.push(e.key.replace("Arrow","").toLowerCase());
+      let newMove = document.createElement('li');
+      newMove.innerText = (userMoves[userMoves.length -1]).toUpperCase();
+      moveList.append(newMove);
+    };
+  });
+
+  moveButton.addEventListener("click", function(e) {
+    executeMove();
+  });
+});
